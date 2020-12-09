7daca537235187b684b1729c1476ee59d59110ff
dnrftrvania <dinar_fitri_27rpl@student.smktelkom-mlg.sch.id>
2020-12-09 17:50:34 +0700

    vue-login

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,46 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import store from '../store'
+import user from '../views/User.vue'
+import Login from '../views/Login.vue'
+import Navbar from '../views/layouts/Navbar.vue'
+import Footer from '../views/layouts/Footer.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/',
+    name: 'user',
+    components: {default: user, header: Navbar, footer: Footer},
+    meta: 
+    {
+      requiresAuth: true
+    }
+  },
 ]
 
 const router = new VueRouter({
-  routes
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: routes
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next()
+      return
+    }
+    next('/login') 
+  } else {
+    next() 
+  }
 })
 
 export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,65 @@
+<template>
+<div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="main-panel">
+        <div class="content-wrapper d-flex align-items-center auth">
+          <div class="row w-100">
+            <div class="col-lg-4 mx-auto">
+              <div class="auth-form-light text-left p-5">
+                <h3>Selamat datang!</h3>
+                <h5 class="font-weight-light">Silahkan Login.</h5>
+                <form v-on:submit.prevent="Login" class="pt-3" method="post" action="#">
+                <div class="form-group">
+                  <div class="input-group">
+                    <div class="input-group-prepend bg-transparent">
+                      <span class="input-group-text bg-transparent border-right-0">
+                        <i class="mdi mdi-account-outline text-primary"></i>
+                      </span>
+                    </div>
+                    <input type="email" class="form-control form-control-lg border-left-0" id="input_email" v-model="email" name="email" placeholder="E-Mail" required>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="input-group">
+                    <div class="input-group-prepend bg-transparent">
+                      <span class="input-group-text bg-transparent border-right-0">
+                        <i class="mdi mdi-lock-outline text-primary"></i>
+                      </span>
+                    </div>
+                    <input type="password" class="form-control form-control-lg border-left-0" name="password" id="input_password" v-model="password" placeholder="Kata Sandi" required>                        
+                  </div>
+                </div>
+                <div class="my-3">
+                  <input type="submit" name="submit" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" value="MASUK">
+                </div>
+              </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- content-wrapper ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+    
+</template>
+<script>
+export default {
+    data() {
+      return {
+        email: '',
+        password: ''
+      }
+    },
+    methods: {
+        Login: function(){
+            let email = this.email 
+            let password = this.password
+            this.$store.dispatch('login', { email, password })
+            .then(() => this.$router.push('/'))
+            .catch(err => console.log(err))
+        }
+    }
+}
+</script>
